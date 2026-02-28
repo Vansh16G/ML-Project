dcae4619d8dea2e5b66b9b84f9e2132419c28d7b
Vansh16G <vanshhalvadiya675@gmail.com>
2026-02-28 09:48:02 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -17,11 +17,6 @@ rfm['Recency'] = rfm['Frequency'].max() - rfm['Frequency']
 
 X = rfm[['Recency','Frequency','Monetary']]
 
-#check cluster
-rfm['Cluster'] = kmeans.labels_
-print("\nCluster Means:")
-print(rfm.groupby('Cluster')[['Recency','Frequency','Monetary']].mean())
-
 # Scale
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
@@ -30,9 +25,19 @@ X_scaled = scaler.fit_transform(X)
 kmeans = KMeans(n_clusters=3, random_state=42)
 kmeans.fit(X_scaled)
 
+# Add cluster column AFTER training
+rfm['Cluster'] = kmeans.labels_
+
+# Show cluster distribution
+print("\nCluster Distribution:")
+print(rfm['Cluster'].value_counts())
+
+# Show cluster means
+print("\nCluster Means:")
+print(rfm.groupby('Cluster')[['Recency','Frequency','Monetary']].mean())
+
 # Save model
 joblib.dump(kmeans, "model.pkl")
 joblib.dump(scaler, "scaler.pkl")
 
-
-print("Model Saved Successfully!")
+print("\nModel Saved Successfully!")
